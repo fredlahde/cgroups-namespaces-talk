5a4f48c0f542ff33200d9732277de67e87910d33
Fred Lahde <fredlahde@gmail.com>
2018-05-28 09:21:38 +0200

    V2

--- code/cgroups.sh
+++ code/cgroups.sh
@@ -1,5 +1,7 @@
-yum install -y libcgroup libcgroup-tools
-systemctl enable cgconfig
-service cgconfig start
-cgexec -g cpu:browser firefox
-cgset -r memory.limit_in_bytes=2G browser
+$ yum install -y libcgroup libcgroup-tools
+$ systemctl enable cgconfig
+$ service cgconfig start
+$ cgexec -g cpu:browser/firefox firefox
+$ cgset -r memory.limit_in_bytes=1G \ 
+	browser/firefox
+$ cgclassify -g cpu,memory:group1 1701
